b54ea60e870956b085034c013f3179a8c1016b4b
zhangjieming0108 <18816799432@163.com>
2019-07-20 17:52:56 +0800

    把页头和页尾添加到了首页，

---
+++ public/js/footer.js
@@ -0,0 +1,15 @@
+$(function(){
+    //请求页头.html片段
+    $.ajax({
+      url:"footer.html",
+      type:"get",
+      success:function(result){
+        //返回的是一段html片段
+        console.log(result);
+        //用片段创建新的<header>元素，替换现有<header id="header">元素
+        $(result).replaceAll("#footer");
+        //并自动添加header.css到页面
+        $(`<link rel="stylesheet" href="css/footer.css">`).appendTo("head");
+      }
+    })
+  });
